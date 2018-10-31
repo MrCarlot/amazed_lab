1123f4d0bbb230789156cdb4147c159b450c11b1
Anton J Olsson <38144418+initGoomba@users.noreply.github.com>
2018-10-31 19:46:00 +0100

    Add files via upload
    Har lagt till comments till de flesta tillägg. Ett problem är att jag var tvungen att ändra public ForkJoinSolver(Maze maze) trots att "You are allowed to use auxiliary (private) methods and attributes, but you should not modify other parts of the given implementation." Konstruktorn används enbart av första spelaren (jag lade till allVisited = new ConcurrentSkipListSet<>(); goalFound = new AtomicBoolean(); där), annars är alternativet att instantieringen får ske direkt i fälten och varje ny child skapar nya allVisited och goalFound innan de får de delade versionerna, vilket är usel programmering :). I klassen står det att man ska använda en ForkJoinPool, vilket antagligen är den definitiva lösningen, men jag vet inte hur isåf.
    
    Om vi skickar in denna och därmed modifierar implementationen (lite), ska vi skriva varför, eller ska vi chansa på att hon inte märker nåt? :)

--- src/main/amazed/solver/ForkJoinSolver.java
+++ src/main/amazed/solver/ForkJoinSolver.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * <code>ForkJoinSolver</code> implements a solver for
@@ -16,10 +17,15 @@ import java.util.concurrent.ConcurrentSkipListSet;
  * <code>ForkJoinPool</code> object.
  */
 
+public class ForkJoinSolver extends SequentialSolver {
 
-public class ForkJoinSolver
-    extends SequentialSolver
-{
+    // Shared by all instances/players
+    private Set<Integer> allVisited;
+    // Needed for not adding cells to allVisited twice
+    private boolean newChild;
+    // If true, stops all players from moving. Wrapped in object so that it can be shared.
+    private AtomicBoolean goalFound;
+
     /**
      * Creates a solver that searches in <code>maze</code> from the
      * start node to a goal.
@@ -29,6 +35,16 @@ public class ForkJoinSolver
     public ForkJoinSolver(Maze maze)
     {
         super(maze);
+        allVisited = new ConcurrentSkipListSet<>();
+        goalFound = new AtomicBoolean();
+    }
+
+    // Auxiliary constructor for children so allVisited and goalFound can be shared
+    private ForkJoinSolver(Maze maze, Set<Integer> allVisited, AtomicBoolean goalFound)
+    {
+        super(maze);
+        this.allVisited = allVisited;
+        this.goalFound = goalFound;
     }
 
     /**
@@ -46,7 +62,6 @@ public class ForkJoinSolver
     {
         this(maze);
         this.forkAfter = forkAfter;
-        this.newChild = false;
     }
 
     /**
@@ -60,75 +75,70 @@ public class ForkJoinSolver
      *           goal node in the maze; <code>null</code> if such a path cannot
      *           be found.
      */
-    protected boolean newChild;
-
-    // Static so that visited cells are shared by all instances/players
-    protected final static Set<Integer> allVisited = new ConcurrentSkipListSet<>();
-    // If true, stops all players from moving
-    private static boolean goalFound;
-
     @Override
-    public List<Integer> compute()
-    {
+    public List<Integer> compute() {
         return parallelSearch();
     }
 
     private List<Integer> parallelSearch() {
         // one player active on the maze at start
-        int player = maze.newPlayer(start);
+        final int player = maze.newPlayer(start);
         // start with start node
         frontier.push(start);
         // as long as not all nodes have been processed
-        while (!frontier.empty() && !(goalFound)) {
+        while (!frontier.empty() && !(goalFound.get())) {
             // get the new node to process
-            int current = frontier.pop();
+            final int current = frontier.pop();
             // if current node has a goal
             if (maze.hasGoal(current) ) {
                 // move player to goal
                 maze.move(player, current);
                 // search finished: reconstruct and return path
                 return pathFromTo(start, current); }
+
             /* if current node has not been visited yet,
              * OR if it is a newly created child (in which case adding it to allVisited has already been
-              * handled in handleForks*/
-            if ((allVisited.add(current) || newChild) && !(goalFound)) {
+             * handled in handleForks*/
+            if ((allVisited.add(current) || newChild) && !(goalFound.get())) {
                 newChild = false;
                 // move player to current node
                 maze.move(player, current);
                 // The path created by appending current instance with its children
                 List<Integer> path = handleForks(current);
                 // If path != null it must be the goal path
-                if (path != null) return path;
+                if (path != null) { return path; }
                 // add neighbour to the nodes to be processed
                 for (int neighbour: maze.neighbors(current)) {
                     frontier.push(neighbour);
                     // if neighbour has not been already visited,
                     // neighbour can be reached from current (i.e., current is nb's predecessor)
                     if (!allVisited.contains(neighbour)) //not thread safe but doesn't matter since players that get-
-                        predecessor.put(neighbour, current); } } }//  -blocked off will eventually return null anyway
+                        { predecessor.putIfAbsent(neighbour, current); } } } } //  -blocked off will eventually return null anyway
         // all nodes explored, no goal found
         return null; }
 
     //Forks children and evaluates if they found the goal
     private List<Integer> handleForks(int current) {
         List<ForkJoinSolver> solvers = new ArrayList<>();
+
         //Only forks at a crossing
         if (maze.neighbors(current).size() > 2) {
             for (int neighbour: maze.neighbors(current)) {
                 if(allVisited.add(neighbour)){ //if possible add the neighbour to allVisited
-                    ForkJoinSolver childSolver = new ForkJoinSolver(maze);
+                    ForkJoinSolver childSolver = new ForkJoinSolver(maze, allVisited, goalFound);
                     childSolver.start = neighbour; //child starts at the neighbour cell
-                    childSolver.newChild = true; //set so that the child wont have to add its cell to allVisited again
+                    childSolver.newChild = true; //set so that the child won't have to add its cell to allVisited again
                     childSolver.fork();
                     solvers.add(childSolver); } }
+
             for (ForkJoinSolver solver : solvers) {
                 List<Integer> result = solver.join();
                 // If goal found, appends child's path to parent's
                 if (result != null) {
                     List<Integer> path = pathFromTo(start, current);
                     path.addAll(result);
-                    goalFound = !goalFound;
-                        return path; }} }
+                    goalFound.set(true);
+                    return path; }} }
         return null;
     }
 }
